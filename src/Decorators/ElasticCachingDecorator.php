ae9b1393653ae60ab998006634a17f55a4409620
alex <alex@intergo.com.cy>
2022-11-30 12:17:28 +0200

    fix key

--- src/Decorators/ElasticCachingDecorator.php
+++ src/Decorators/ElasticCachingDecorator.php
@@ -23,6 +23,19 @@ abstract class ElasticCachingDecorator extends AbstractCachingDecorator
      */
 
     /**
+     * @param $function
+     * @param null $arguments
+     * @return string
+     */
+    protected function key($function, $arguments = null)
+    {
+        if (empty($arguments)) {
+            return $function;
+        }
+        return $this->advanceKey($function, $arguments);
+    }
+
+    /**
      * @param array $params
      * @return Hit
      */
